178cd429bc9775addc616e207b5559434a2b4f57
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2020-10-05 23:53:32 +0200

    Initial composition

--- coding-exercise/code/7-composition.ts
+++ coding-exercise/code/7-composition.ts
@@ -1,1 +1,27 @@
-// TODO
+interface VehicleConfig {
+  model: string;
+  fuel: number;
+  color: string;
+}
+
+const driver = (state: VehicleConfig) => {
+    return {
+        drive: () => console.log(`${state.model} drove`);
+    }
+};
+
+const flyer = (state: VehicleConfig) => {
+    return {
+        fly: () => console.log(`${state.model} flew`);
+    }
+};
+
+const carFactory = (model: string, color: string) => {
+  let state: VehicleConfig = {
+    model,
+    color,
+    fuel: 100,
+  };
+
+  return { ...state, ...driver(state) };
+};
