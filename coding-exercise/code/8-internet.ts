2133e45167061f0c981d1e16bef7eb50e9b59c90
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2020-10-06 00:17:13 +0200

    Internet stuff

--- coding-exercise/code/8-internet.ts
+++ coding-exercise/code/8-internet.ts
@@ -1,1 +1,24 @@
-// TODO
+// Needed to redefine because of ts-node shit
+const fetchStuff = require("node-fetch");
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+fetchStuff("https://jsonplaceholder.typicode.coma/todos")
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response;
+  })
+  .then((res) => res.json())
+  .then((todos: Array<Todo>) => {
+    todos.forEach((item) => console.log(item));
+  })
+  .catch((e) => {
+    console.log(e.message);
+  });
