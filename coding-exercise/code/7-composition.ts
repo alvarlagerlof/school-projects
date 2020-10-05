c6f1475ece418c74a9f9c04c6df7bead8e634b55
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2020-10-05 23:58:38 +0200

    Composition done

--- coding-exercise/code/7-composition.ts
+++ coding-exercise/code/7-composition.ts
@@ -5,15 +5,15 @@ interface VehicleConfig {
 }
 
 const driver = (state: VehicleConfig) => {
-    return {
-        drive: () => console.log(`${state.model} drove`);
-    }
+  return {
+    drive: () => console.log(`A ${state.color} ${state.model} drove`),
+  };
 };
 
 const flyer = (state: VehicleConfig) => {
-    return {
-        fly: () => console.log(`${state.model} flew`);
-    }
+  return {
+    fly: () => console.log(`A ${state.color} ${state.model} flew`),
+  };
 };
 
 const carFactory = (model: string, color: string) => {
@@ -25,3 +25,33 @@ const carFactory = (model: string, color: string) => {
 
   return { ...state, ...driver(state) };
 };
+
+const planeFactory = (model: string, color: string) => {
+  let state: VehicleConfig = {
+    model,
+    color,
+    fuel: 100,
+  };
+
+  return { ...state, ...flyer(state) };
+};
+
+const flyingCarFactory = (model: string, color: string) => {
+  let state: VehicleConfig = {
+    model,
+    color,
+    fuel: 100,
+  };
+
+  return { ...state, ...driver(state), ...flyer(state) };
+};
+
+const car = carFactory("Volvo V70", "red");
+car.drive();
+
+const plane = planeFactory("Cessna 172", "green");
+plane.fly();
+
+const flyingCar = flyingCarFactory("Hyundai eVTOL", "white");
+flyingCar.drive();
+flyingCar.fly();
